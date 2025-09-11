473d16504b059aca3619cb7b649fbe41c0d09f00
Abdullah Al Maruf Supto <124609636+marufsupto@users.noreply.github.com>
2025-09-11 13:02:27 +0600

    Add new-file.txt

---
+++ NumericSort.js
@@ -0,0 +1,4 @@
+const roll_numbers = [3,5,7,1,4];
+console.log(roll_numbers.sort(function(a,b){
+    return a-b;
+}))
---
+++ sort.js
@@ -0,0 +1,2 @@
+const students = ["Supto","Akash","Saad","Daiyan"];
+console.log(students.sort());
