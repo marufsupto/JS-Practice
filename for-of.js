ee8f3bdb6c286e20cb939f0b3053d359e8f304b0
Abdullah Al Maruf Supto <124609636+marufsupto@users.noreply.github.com>
2025-09-15 13:30:01 +0600

    added a new file

---
+++ for-of.js
@@ -0,0 +1,4 @@
+let a = [4,5,10,7,6,2];
+for(x of a){
+    console.log(x);
+}
---
+++ while_loop.js
@@ -0,0 +1,7 @@
+let i = 0;
+let text = "";
+while(i<10){
+    text += "The number is:"  +i;
+    i++;
+}
+console.log(text);
