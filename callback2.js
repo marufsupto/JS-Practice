4a07362d751deed06667e78c86ebbd7820586dc4
Abdullah Al Maruf Supto <124609636+marufsupto@users.noreply.github.com>
2025-09-22 12:47:55 +0600

    added a new file

---
+++ callback2.js
@@ -0,0 +1,10 @@
+function calculator(num1,num2,callback){
+        let sum = num1 + num2;
+        if(callback){
+            callback(sum);
+            return sum;
+        }
+}
+calculator(5,5, function(result){
+    console.log(result);
+})
